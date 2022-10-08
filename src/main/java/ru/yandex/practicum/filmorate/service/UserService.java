812c394b5fef4c6be9f37af5b014fadd1ff47bc0
Grayroof <1grayroof@gmail.com>
2022-10-08 20:39:58 +0300

    comment: добавить комментарии к публичным методам в UserService

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.FilmValidationException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.UserValidationException;
 import ru.yandex.practicum.filmorate.exception.WrongIdException;
@@ -29,6 +30,95 @@ public class UserService {
         this.userStorage = userStorage;
     }
 
+    /**
+     * Возвращает коллекцию пользователей
+     * */
+    public Collection<User> getAllUsers() {
+        return userStorage.getAllUsers();
+    }
+
+    /**
+     * Добавляет пользователя в коллекцию
+     * Возвращает добавленного пользователя
+     * @exception UserValidationException в случае, если пользователь содержит недопустимое содержание полей
+     * */
+    public User add(final User user) {
+        validate(user);
+        return userStorage.addUser(user);
+    }
+
+    /**
+     * Обновляет пользователя в коллекции
+     * Возвращает обновленного пользователя
+     * @exception UserValidationException в случае, если пользователь содержит недопустимое содержание полей
+     * */
+    public User update(final User user) {
+        validate(user);
+        return userStorage.updateUser(user);
+    }
+
+    /**
+     * Добавляет пользователя в друзья другому пользователю
+     * @param supposedUserId - идентификатор пользователя
+     * @param supposedFriendId - идентификатор друга
+     * */
+    public void addFriend(final String supposedUserId, final String supposedFriendId) {
+        User user = getStoredUser(supposedUserId);
+        User friend = getStoredUser(supposedFriendId);
+        user.addFriend(friend.getId());
+        friend.addFriend(user.getId());
+    }
+
+    /**
+     * Удаляет пользователя из друзей другого пользователя
+     * @param supposedUserId - идентификатор пользователя
+     * @param supposedFriendId - идентификатор друга
+     * */
+    public void deleteFriend(final String supposedUserId, final  String supposedFriendId) {
+        User user = getStoredUser(supposedUserId);
+        User friend = getStoredUser(supposedFriendId);
+        user.deleteFriend(friend.getId());
+        friend.deleteFriend(user.getId());
+    }
+
+    /**
+     * Возвращает коллекцию пользователей, которые являются друзьями для заданного пользователя
+     * @param supposedUserId - идентификатор пользователя
+     * */
+    public Collection<User> getFriends(final String supposedUserId) {
+        User user = getStoredUser(supposedUserId);
+        Collection<User> friends = new HashSet<>();
+        for (Integer id : user.getFriends()) {
+            friends.add(userStorage.getUser(id));
+        }
+        return friends;
+    }
+
+    /**
+     * Возвращает коллекцию пользователей, которые являются общими друзьями двух заданных пользователей
+     * @param supposedUserId - идентификатор пользователя
+     * @param supposedOtherId - идентификатор другого пользователя
+     * */
+    public Collection<User> getCommonFriends(final String supposedUserId, final String supposedOtherId) {
+        User user = getStoredUser(supposedUserId);
+        User otherUser = getStoredUser(supposedOtherId);
+        Collection<User> commonFriends = new HashSet<>();
+        for (Integer id : user.getFriends()) {
+            if (otherUser.getFriends().contains(id)) {
+                commonFriends.add(userStorage.getUser(id));
+            }
+        }
+        return commonFriends;
+    }
+
+    /**
+     * Возвращает пользователя по идентификатора
+     * @param supposedId - идентификатор пользователя
+     * */
+    public User getUser(final String supposedId) {
+        return getStoredUser(supposedId);
+    }
+
     private void validate(final User user) {
         if(user.getName() == null) {
             user.setName(user.getLogin());
@@ -49,59 +139,6 @@ public class UserService {
         if (user.getId() == 0) {
             user.setId(++increment);
         }
-    }
-
-    public Collection<User> getAllUsers() {
-        return userStorage.getAllUsers();
-    }
-
-    public User add(final User user) {
-        validate(user);
-        return userStorage.addUser(user);
-    }
-
-    public User update(final User user) {
-        validate(user);
-        return userStorage.updateUser(user);
-    }
-
-    public void addFriend(final String supposedUserId, final String supposedFriendId) {
-        User user = getStoredUser(supposedUserId);
-        User friend = getStoredUser(supposedFriendId);
-        user.addFriend(friend.getId());
-        friend.addFriend(user.getId());
-    }
-
-    public void deleteFriend(final String supposedUserId, final  String supposedFriendId) {
-        User user = getStoredUser(supposedUserId);
-        User friend = getStoredUser(supposedFriendId);
-        user.deleteFriend(friend.getId());
-        friend.deleteFriend(user.getId());
-    }
-
-    public Collection<User> getFriends(final String supposedUserId) {
-        User user = getStoredUser(supposedUserId);
-        Collection<User> friends = new HashSet<>();
-        for (Integer id : user.getFriends()) {
-            friends.add(userStorage.getUser(id));
-        }
-        return friends;
-    }
-
-    public Collection<User> getCommonFriends(final String supposedUserId, final String supposedOtherId) {
-        User user = getStoredUser(supposedUserId);
-        User otherUser = getStoredUser(supposedOtherId);
-        Collection<User> commonFriends = new HashSet<>();
-        for (Integer id : user.getFriends()) {
-            if (otherUser.getFriends().contains(id)) {
-                commonFriends.add(userStorage.getUser(id));
-            }
-        }
-        return commonFriends;
-    }
-
-    public User getUser(final String supposedId) {
-        return getStoredUser(supposedId);
     }
 
     private Integer idFromString(final String supposedId) {
@@ -125,7 +162,4 @@ public class UserService {
         }
         return user;
     }
-
-
-
 }
