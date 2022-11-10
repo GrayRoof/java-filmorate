90d3a07862d860d3089b7bce782fad9ca8cf31ff
Grayroof <1grayroof@gmail.com>
2022-11-10 21:53:45 +0300

    feat: добавить интеграционные тесты DBFilmStorageTest

--- src/test/java/ru/yandex/practicum/filmorate/storage/dao/DBFilmStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/dao/DBFilmStorageTest.java
@@ -7,8 +7,11 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,7 +24,7 @@ class DBFilmStorageTest {
     private final DBFilmStorage filmStorage;
 
     @Test
-    public void testFindFilmById() {
+    public void testGetFilmById() {
 
         Film createFilm = new Film();
         createFilm.setMpa(new Mpa(1,"mpa", "desc"));
@@ -37,34 +40,76 @@ class DBFilmStorageTest {
     }
 
     @Test
-    void getFilm() {
-    }
-
-    @Test
     void getAllFilms() {
-    }
+        Film first = new Film(0,
+                "first",
+                "first description",
+                LocalDate.now().minusYears(8),
+                90L,
+                3,
+                new Mpa(1,"o","o"),
+                new ArrayList<>(),
+                new ArrayList<>());
+        Film second = new Film(0,
+                "second",
+                "second description",
+                LocalDate.now().minusYears(15),
+                100L,
+                2,
+                new Mpa(3,"o","o"),
+                new ArrayList<>(),
+                new ArrayList<>());
+        filmStorage.addFilm(first);
+        filmStorage.addFilm(second);
 
-    @Test
-    void addFilm() {
+        Collection<Film> dbFilms = filmStorage.getAllFilms();
+        assertEquals(2, dbFilms.size());
     }
 
     @Test
     void updateFilm() {
+        Film first = new Film(0,
+                "first",
+                "first description",
+                LocalDate.now().minusYears(8),
+                90L,
+                3,
+                new Mpa(1,"o","o"),
+                new ArrayList<>(),
+                new ArrayList<>());
+        Film added = filmStorage.addFilm(first);
+        added.setName("update");
+        filmStorage.updateFilm(added);
+        Film dbFilm = filmStorage.getFilm(added.getId());
+        assertThat(dbFilm).hasFieldOrPropertyWithValue("name", "update");
     }
 
     @Test
     void deleteFilm() {
-    }
+        Film first = new Film(0,
+                "first",
+                "first description",
+                LocalDate.now().minusYears(8),
+                90L,
+                3,
+                new Mpa(1,"o","o"),
+                new ArrayList<>(),
+                new ArrayList<>());
+        Film second = new Film(0,
+                "second",
+                "second description",
+                LocalDate.now().minusYears(15),
+                100L,
+                2,
+                new Mpa(3,"o","o"),
+                new ArrayList<>(),
+                new ArrayList<>());
+        Film addedFirst = filmStorage.addFilm(first);
+        Film addedSecond = filmStorage.addFilm(second);
 
-    @Test
-    void addLike() {
-    }
-
-    @Test
-    void deleteLike() {
-    }
-
-    @Test
-    void getMostPopularFilms() {
+        Collection<Film> beforeDelete = filmStorage.getAllFilms();
+        filmStorage.deleteFilm(addedFirst);
+        Collection<Film> afterDelete = filmStorage.getAllFilms();
+        assertEquals(beforeDelete.size() - 1, afterDelete.size());
     }
 }
